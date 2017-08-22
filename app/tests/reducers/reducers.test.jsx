0b909ec6f65328297db274459d117c775a66d160
dpGian <dplgian@gmail.com>
2017-08-22 18:01:02 +0100

    Add todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -26,4 +26,37 @@ describe('Reducers', () => {
     });
   });
 
+  describe('todosReducer', () => {
+    it('should add new Todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'sample text'
+      };
+      var res = reducers.todosReducer(df([]),df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    it('should toggle Todo', () => {
+      var todos = [{
+        id: '12',
+        text: 'Some text',
+        completed: true,
+        createdAt: 123,
+        completedAt: 125
+      }];
+
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: '12'
+      };
+      var res = reducers.todosReducer(df(todos),df(action));
+
+      expect(res[0].completed).toEqual(false);
+      expect(res[0].completedAt).toEqual(undefined);
+    });
+
+  });
+
 });
